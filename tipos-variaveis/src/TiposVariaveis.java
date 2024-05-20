4890f1f6101f8a0604dda78891adf495a6554d78
Alenda_Matheus31 <matheusassis3103@gmail.com>
2024-05-20 13:26:41 -0300

    alteracao de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
+       // short numeroCurto = 1;
+       // int numeroNormal = numeroCurto;
+       // short numeroCurto2 = (short) numeroNormal; // cast meio que avisaa quando for atribuir um tipo a outro para não gerar erro de espaço na variavel.
+        
+        int numero = 1;
+
+        numero = 2;
+        System.out.println(numero);
     }
 }
